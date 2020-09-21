3a48e30c0d22c9f095cd4c2dd00c3a29e14ffa10
Sesbenty <sasha.shanin.01@mail.ru>
2020-09-21 23:24:01 +0300

    Геометрия

---
+++ Task/Geometry/Matrix4x4.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task.Geometry
+{
+    struct Matrix4x4
+    {
+        float[,] vs;
+
+        public Matrix4x4()
+        {
+            vs = new float[4, 4];
+        }
+
+        public float this[int x, int y]
+        {
+            get { return vs[x, y]; }
+            set { vs[x, y] = value; }
+        }
+    }
+}
---
+++ Task/Geometry/Quaternion.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task.Geometry
+{
+    struct Quaternion
+    {
+        float w { get; set; }
+        float x { get; set; }
+        float y { get; set; }
+        float z { get; set; }
+
+        public Quaternion(float w, float x, float y, float z)
+        {
+            this.w = w;
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+    }
+}
