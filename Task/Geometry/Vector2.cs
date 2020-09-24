0f99285b28bfca455fafb1a94450d513ea5903ab
Sesbenty <71658364+Sesbenty@users.noreply.github.com>
2020-09-24 12:01:29 +0300

    a
    Создал Vector2 и интерфейс Mathf

---
+++ Task/Geometry/Vector2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task.Geometry
+{
+    class Vector2
+    {
+        public float x { get; set; } 
+        public float y { get; set; }
+
+        public Vector2(float x, float y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public static float Distance(Vector2 a, Vector2 b)
+        {
+            return (float)Math.Sqrt(a.x * b.x + a.y * b.y);
+        }
+    }
+}
---
+++ Task/Mathf.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task
+{
+    static class Mathf
+    {
+        static float Sqrt(float a)
+        {
+            float x = a;
+            float c = 1;
+
+            return 0;
+        }
+
+        static float Cos(float a)
+        {
+            return 0;
+        }
+
+        static float Sin(float a)
+        {
+            return 0;
+        }
+    }
+}
