3af50d42fa41160f6ec16f58afe30a5d0f946fca
Sesbenty <71658364+Sesbenty@users.noreply.github.com>
2020-09-24 14:25:07 +0300

    MoveObj

---
+++ Task/MoveObj.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task.Geometry;
+using Task.Objects;
+namespace Task
+{
+    class MoveObj:GameScript
+    {
+        Vector2 a, b;
+        public MoveObj(GameObject parent,Vector2 a, Vector2 b) : base(parent)
+        {
+
+        }
+    }
+}
